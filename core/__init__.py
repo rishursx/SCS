3b4f8e28d579521cc5a5e01962f783a9a50d431f
Rishu Trivedi <rishu.trivedi1997@gmail.com>
2019-07-13 18:34:12 +0530

    Djinja enters the scene

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -10,5 +10,7 @@ def create_app(test_config=None):
 
     @app.route('/')
     def welcome():
-        return('Lets resolve your issue')
+        naem='Rishu'
+        return render_template('welcome.html')
     return app
+
